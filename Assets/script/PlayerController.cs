327dc010427525916189208a449359038e3e6124
shianblue <169328091+shianblue@users.noreply.github.com>
2026-07-31 19:37:56 +0900

    AddTorqueを使用してROTATE機能を実装,未使用のScriptをArchiveフォルダに移行

--- Assets/script/PlayerController.cs
+++ Assets/script/PlayerController.cs
@@ -1,4 +1,3 @@
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 public class PlayerController : MonoBehaviour
@@ -9,6 +8,9 @@ public class PlayerController : MonoBehaviour
     
     [SerializeField] public float thrustForceMove;
     [SerializeField] private float thrustForceReversMove;
+
+    [SerializeField] private float thrustForceTorque;
+    [SerializeField] private float thrustForceReversTorque;
     
     public Rigidbody rb;
     
@@ -26,21 +28,17 @@ public class PlayerController : MonoBehaviour
     void FixedUpdate()
     {
         thrustForceMove = slider.value;
-        Debug.Log(thrustForceMove);
         Move();
-        //Debug.Log(wsValue);
     }
 
-    public void Move()
+    private void Move()//WASDの操作
     {
-        int wsValue = 0;
-        int adValue = 0;
         int spcValue = 0;
         //==========================================
-        //                 　W/S
+        //                MOVE_W/S
         //==========================================
-        float forwardSpeed = Vector3.Dot(rb.linearVelocity, transform.forward);
-        float RLSpeed = Vector3.Dot(rb.linearVelocity, transform.right);
+        int wsValue = 0;
+        float forwardDot = Vector3.Dot(rb.linearVelocity, transform.forward);
         if (Input.GetKey(KeyCode.W))
         {
             wsValue += 1;
@@ -55,59 +53,72 @@ public class PlayerController : MonoBehaviour
         {
             rb.AddForce(transform.forward * (wsValue * thrustForceMove), ForceMode.Force);
         }
-        else if (Mathf.Abs(forwardSpeed) > 0.1f)
+        else if (Mathf.Abs(forwardDot) > 0.1f)
         {
-            rb.AddForce(-transform.forward *(thrustForceReversMove * Mathf.Sign(forwardSpeed)), ForceMode.Force);
+            rb.AddForce(-transform.forward *(thrustForceReversMove * Mathf.Sign(forwardDot)), ForceMode.Force);
         }
         else
         {
-            rb.linearVelocity -= transform.forward * forwardSpeed;
-            //rb.linearVelocity = new Vector3(0,0,0);
+            rb.linearVelocity -= transform.forward * forwardDot;//FixedUpdateを用いても速度がちょうど0のときに静止が出来なかったため最終制動のみ直接操作
         }
         //==========================================
-        //                 　A/D
+        //                MOVE_A/D
         //==========================================
+        int adValue = 0;
+        float rlDot = Vector3.Dot(rb.linearVelocity, transform.right);
         if (Input.GetKey(KeyCode.A))
         {
-            adValue = 1;
+            adValue -= 1;
         }
 
         if (Input.GetKey(KeyCode.D))
         {
-            adValue = -1;
+            adValue += 1;
         }
 
         if (adValue != 0)
         {
-            rb.AddForce(-transform.right * (adValue * thrustForceMove), ForceMode.Force);
+            rb.AddForce(transform.right * (adValue * thrustForceMove), ForceMode.Force);
         }
-        else if (Mathf.Abs(RLSpeed) > 0.1f)
+        else if (Mathf.Abs(rlDot) > 0.1f)
         {
-            rb.AddForce(transform.right * (thrustForceReversMove * -Mathf.Sign(RLSpeed)), ForceMode.Force);
+            rb.AddForce(transform.right * (thrustForceReversMove * -Mathf.Sign(rlDot)), ForceMode.Force);
         }
         else
         {
-            //rb.linearVelocity = new Vector3(0,0,0);
-            rb.linearVelocity -= transform.right * RLSpeed;
+            rb.linearVelocity -= transform.right * rlDot;
         }
-    }
-}
-/*if (Input.GetKey(KeyCode.W))
+        //==========================================
+        //               ROTATE_Q/E
+        //==========================================
+        int qeValue = 0;
+        float qeDot = Vector3.Dot(rb.angularVelocity, transform.up);
+        if (Input.GetKey(KeyCode.Q))
         {
-            time += Time.deltaTime;
-            if (time > 0.2)
-            {
-                wsValue += 1f;
-                time = 0;
-            }
+            qeValue -= 1;
         }
 
-        if (Input.GetKey(KeyCode.S))
+        if (Input.GetKey(KeyCode.E))
         {
-            time += Time.deltaTime;
-            if (time > 0.2)
-            {
-                wsValue -= 1f;
-                time = 0;
-            }
-        }*/
+            qeValue += 1;
+        }
+
+        if (qeValue != 0)
+        {
+            rb.AddTorque(transform.up * (qeValue * thrustForceTorque), ForceMode.Force);
+        }
+        else if (Mathf.Abs(qeDot) > 0.1f)
+        {
+            rb.AddTorque(transform.up*(thrustForceTorque * -Mathf.Sign(qeDot)), ForceMode.Force);
+        }
+        else
+        {
+            rb.angularVelocity -= transform.up * qeDot;
+        }
+        //X軸のROTATEを追加するかはステージ作成後決定
+        //==========================================
+        //              MOVE_SPACE/C
+        //==========================================
+        
+    }
+}
