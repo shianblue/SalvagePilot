55f85dbbe86fd4639bdc9e1ef1bb17fb51e96f31
shianblue <169328091+shianblue@users.noreply.github.com>
2026-08-28 19:46:33 +0900

    目標を表示させる機能の準備

--- Assets/script/UIManager.cs
+++ Assets/script/UIManager.cs
@@ -4,7 +4,18 @@ public class UIManager : MonoBehaviour
 {
     private Delivery.DeliveryStatus _deliveryStatus;
     public TextMeshProUGUI statusText;
-    
+    public TextMeshProUGUI goalsStatusText;
+
+    private enum Goals
+    {
+        Phase1,
+        Phase2,
+        Phase3,
+        Phase4,
+        Phase5
+    }
+
+    private Goals _goals;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -15,5 +26,23 @@ public class UIManager : MonoBehaviour
     void Update()
     {
         statusText.text = $"Status: {_deliveryStatus}";
+        switch (_goals)
+        {
+            case Goals.Phase1:
+                goalsStatusText.text = "ドローンを追う";//プレイヤーの誘導をUI依存にするのかドローンを出してtailで追わせるのか決めてないのでどっちでも
+                break;
+            case Goals.Phase2:
+                goalsStatusText.text = "デブリを探す";//これもどうなるかわからないの変更があれば変わる
+                break;
+            case Goals.Phase3:
+                goalsStatusText.text = "デブリを回収する(Fボタン)";
+                break;
+            case Goals.Phase4:
+                goalsStatusText.text = "回収地点へ向かう";
+                break;
+            case Goals.Phase5:
+                goalsStatusText.text = "デブリを降ろす(Fボタン)";
+                break;
+        }
     }
 }
